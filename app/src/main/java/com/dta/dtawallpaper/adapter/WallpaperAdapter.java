8177fa330ee49d0fef01e2ce1e176833106e2033
heiyu-sec <304429477@qq.com>
2023-06-16 15:40:35 +0800

    add item_wallpaper

--- app/src/main/java/com/dta/dtawallpaper/adapter/WallpaperAdapter.java
+++ app/src/main/java/com/dta/dtawallpaper/adapter/WallpaperAdapter.java
@@ -1,33 +1,43 @@
 package com.dta.dtawallpaper.adapter;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.dta.dtawallpaper.R;
 import com.dta.dtawallpaper.entity.WallPaperEntity;
 
 import java.util.List;
 
 public class WallpaperAdapter extends BaseAdapter {
+    Context context;
     List<WallPaperEntity> wallPaperEntityList;
+
+    public WallpaperAdapter(Context context, List<WallPaperEntity> wallPaperEntityList) {
+        this.context = context;
+        this.wallPaperEntityList = wallPaperEntityList;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return wallPaperEntityList.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public Object getItem(int position) {
+        return wallPaperEntityList.get(position);
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        View.inflate(context, R.layout.item_wallpaper,null);
+
         return null;
     }
 }
